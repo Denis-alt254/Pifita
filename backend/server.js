baf0f4b702f2e976cd27f57e6976800a0654d103
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-05-12 14:02:14 +0300

    routes and database connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 require('dotenv').config();
+const cors = require("cors");
+const userRoute = require('./routers/userRouter');
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+//connect to database
+connectDB();
+
+//Routes
+app.use('/api/user', userRoute);
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on http://localhost:${process.env.PORT}`)
