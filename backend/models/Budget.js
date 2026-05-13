02576f78e08fcfa334f71542edfd4712540545c4
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-05-13 09:36:55 +0300

    added userid attribute

--- backend/models/Budget.js
+++ backend/models/Budget.js
@@ -1,6 +1,7 @@
 const {default: mongoose} = require("mongoose");
 
 const BudgetSchema = new mongoose.Schema({
+    userId:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     category: String,
     to_spent: Number
 }, {timestamps: true});
--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -3,6 +3,7 @@ const {default: mongoose} = require("mongoose");
 const ExpenseSchema = new mongoose.Schema({
     amount: Number,
     category: [{type: mongoose.Schema.Types.ObjectId, ref: 'Budget'}],
+    userId:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     note: String
 }, {timestamps: true});
 
