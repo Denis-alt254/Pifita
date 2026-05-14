8801d6030f4d7d3baefd27eca38c92f3470211a3
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-05-14 20:01:19 +0300

    update budget route

--- backend/routers/budgetRoutes.js
+++ backend/routers/budgetRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
-const { createBudget } = require('../controllers/budgetController');
+const { createBudget, updateBudget } = require('../controllers/budgetController');
 const router = express.Router();
 
 router.post('/create', authMiddleware, createBudget);
+router.post('/update', authMiddleware, updateBudget);
 
 module.exports = router;
