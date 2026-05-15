79e3822a8493df32e4d9c2a83eda1a299f232747
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-05-15 08:34:06 +0300

    get budgets per user route

--- backend/routers/budgetRoutes.js
+++ backend/routers/budgetRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
-const { createBudget, updateBudget, deleteBudget } = require('../controllers/budgetController');
+const { createBudget, updateBudget, deleteBudget, getBudgetsByUser, getAllBudgets } = require('../controllers/budgetController');
 const router = express.Router();
 
 router.post('/create', authMiddleware, createBudget);
 router.patch('/update/:id', authMiddleware, updateBudget);
 router.delete('/delete/:id', authMiddleware, deleteBudget);
+router.get('/', authMiddleware, getBudgetsByUser);
 
 module.exports = router;
