6e8700308742a60e95e55428407826b65123dadc
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-05-16 12:27:02 +0300

    updating get budgets userid

--- backend/controllers/budgetController.js
+++ backend/controllers/budgetController.js
@@ -2,7 +2,7 @@ const Budget = require("../models/Budget");
 
 const getBudgetsByUser = async(req, res) => {
     try {
-        const userId = req.user._id;
+        const userId = req.user.userId;
         const budgets = await Budget.find({userId: userId});
         res.status(200).json(budgets);
     } catch (error) {
